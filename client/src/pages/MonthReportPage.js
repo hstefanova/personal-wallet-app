32999c18571be87c352e00f36ca388e0503dabc3
hstefanova <hstefanova@github.com>
2020-11-19 17:51:49 +0200

    add page placeholders

---
+++ client/src/pages/MonthReportPage.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const MonthReportPage = () => {
+  return <div>MonthReportPage page goes here</div>;
+};
+
+export default MonthReportPage;
---
+++ client/src/pages/UserCharts.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ChartsPage = () => {
+  return <div>ChartsPage page goes here</div>;
+};
+
+export default ChartsPage;
